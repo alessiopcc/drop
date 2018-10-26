861a8be68d817d99b671fa0fadff7181183c8d83
lromerio <lucio.romerio@epfl.ch>
2018-10-26 22:46:30 +0200

    Test math/max, close #2
    - Implicit cast does not compile
    - Incoherent specialization does not compile

---
+++ drop/test/utils/math.cpp
@@ -0,0 +1,40 @@
+#include "framework/test.hpp"
+
+// Libraries
+
+#include <iostream>
+
+// Includes
+
+#include "utils/math.hpp"
+
+namespace
+{
+    // Using
+
+    using namespace drop;
+
+    // Methods
+
+    template <int value> size_t identity()
+    {
+        return value;
+    };
+
+    // Tests
+
+    $test("math/max", []
+    {
+        if(identity <max({1, 3, 2, -1})>() != 3)
+            throw "`max({1, 3, 1})` does not return 3.`";
+
+        if(identity <max({sizeof(int), sizeof(short), sizeof(char)})>() != 4)
+            throw "`max(sizeof(int), sizeof(short), sizeof(char))` does not return 4.`";
+
+        if(identity <max({-1, -3, -2, -5})>() != -1)
+            throw "`max({1, 3, 1})` does not return -1.`";
+
+        if(identity <max({'r', 'a', 'i', 'n'})>() != (int)'r')
+            throw "`max({'r', 'a', 'i', 'n'})` does not return `(int)'r'`";
+    });
+};
