e3087b039cb3a86dfe917cdf52657afd3552626e
lromerio <lucio.romerio@epfl.ch>
2018-11-30 12:22:34 +0100

    Test crypto/shorthash

--- drop/test/crypto/shorthash.cpp
+++ drop/test/crypto/shorthash.cpp
@@ -31,18 +31,47 @@ namespace
 
     // Tests
 
-    $test("shorthash/develop", []
+    $test("shorthash", []
     {
-        shorthash shorthash;
+        {
+            shorthash shorthash;
 
-        std :: cout << shorthash(44) << std :: endl;
-        std :: cout << shorthash(44) << std :: endl;
+            if(shorthash(44) != shorthash(44))
+                throw "Hashing the same value with the same `shorthash` gives different outputs.";
 
-        std :: cout << shorthash(std :: string("extremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstring")) << std :: endl;
+            auto myhash = shorthash(std :: string("extremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstring"));
 
-        std :: cout << shorthash(std :: string("extremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstring")) << std :: endl;
+            auto myotherhash = shorthash(std :: string("extremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstringextremelylongstring"));
 
-        std :: cout << shorthash(myclass()) << std :: endl;
-        std :: cout << shorthash(myclass()) << std :: endl;
+            if(myhash != myotherhash)
+                throw "Hashing the same extremly long string with the same `shorthash` gives different outputs.";
+
+            if(shorthash(myclass()) != shorthash(myclass()))
+                throw "Hashing the same object with the same `shorthash` gives different outputs";
+        }
+
+        {
+            shorthash myshorthash;
+            shorthash myothershorthash;
+
+            if(myshorthash(33) == myothershorthash(33))
+                throw "Hashing the same value with different `shorthash` gives the same output.";
+
+            if(myshorthash(std :: string("rain")) == myothershorthash(std :: string("rain")))
+                throw "Hashing the same string with different `shorthash` gives the same output.";
+
+            if(myshorthash(myclass()) == myothershorthash(myclass()))
+                throw "Hashing the same object with different `shorthash` gives the same output.";
+        }
+
+        {
+            shorthash shorthash;
+
+            if(shorthash(3) == shorthash(33))
+                throw "Hashing the different values with the same `shorthash` gives the same output.";
+
+            if(shorthash(std :: string("rain")) == shorthash(std :: string("Rain")))
+                throw "Hashing different string with the same `shorthash` gives the same output.";
+        }
     });
 };
