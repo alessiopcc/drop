724112a67d877937cbcd62246a8ecc880e483fa4
lromerio <lucio.romerio@epfl.ch>
2019-03-11 01:19:49 +0100

    Update network/connection test
    Add tests for `securesync`, `secureasync` and `secure`.

--- drop/test/network/connection.cpp
+++ drop/test/network/connection.cpp
@@ -130,4 +130,478 @@ namespace
             goodbye.receivesync <std :: string> ();
         }
     });
+
+    $test("connection/securesync", {.instances = 6},[]
+    {
+        std :: string message = "Hello world!";
+        std :: vector <uint8_t> vector = {3, 2, 1, 0};
+
+        keyexchanger local;
+
+        if(:: test :: instance :: id() == 0)
+        {
+            auto listener = tcp :: listen(2234);
+            auto connection = listener.acceptsync();
+
+            connection.securesync <peer> (local);
+
+            auto receivedmessage = connection.receivesync <std :: string> ();
+            if(receivedmessage != message)
+                throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+            auto receivedvector = connection.receivesync <std :: vector <uint8_t>>();
+            if(receivedvector != vector)
+                throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+        }
+        else if(:: test :: instance :: id() == 1)
+        {
+            sleep(1_s);
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (0), 2234});
+
+            connection.securesync <peer> (local);
+
+            connection.sendasync(message);
+            connection.sendasync(vector);
+        }
+        else if(:: test :: instance :: id() == 2)
+        {
+            auto listener = tcp :: listen(2234);
+            auto connection = listener.acceptsync();
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+
+            connection.securesync <server> (local);
+
+            connection.sendasync(message);
+
+            auto receivedvector = connection.receivesync <std :: vector <uint8_t>>();
+            if(receivedvector != vector)
+                throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+        }
+        else if(:: test :: instance :: id() == 3)
+        {
+            sleep(1_s);
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (2), 2234});
+
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            connection.securesync <client> (local, remote);
+
+            auto receivedmessage = connection.receivesync <std :: string> ();
+            if(receivedmessage != message)
+                throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+            connection.sendasync(vector);
+        }
+        else if(:: test :: instance :: id() == 4)
+        {
+            auto listener = tcp :: listen(2234);
+            auto connection = listener.acceptsync();
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            connection.securesync <peer> (local, remote);
+
+            connection.sendasync(message);
+
+            auto receivedvector = connection.receivesync <std :: vector <uint8_t>>();
+            if(receivedvector != vector)
+                throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+        }
+        else
+        {
+            sleep(1_s);
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (4), 2234});
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            connection.securesync <peer> (local, remote);
+
+            auto receivedmessage = connection.receivesync <std :: string> ();
+            if(receivedmessage != message)
+                throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+            connection.sendasync(vector);
+        }
+    });
+
+    $test("connection/secureasync", {.instances = 7},[]
+    {
+        std :: string message = "Hello world!";
+        std :: vector <uint8_t> vector = {3, 2, 1, 0};
+
+        std :: string bye = "Bye.";
+
+        keyexchanger local;
+
+        if(:: test :: instance :: id() == 0)
+        {
+            auto listener = tcp :: listen(2235);
+            auto connection = listener.acceptsync();
+
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2235});
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secureasync <peer> (local);
+
+                auto receivedmessage = co_await connection.receiveasync <std :: string> ();
+                if(receivedmessage != message)
+                    throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+                auto receivedvector = co_await connection.receiveasync <std :: vector <uint8_t>>();
+                if(receivedvector != vector)
+                    throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 1)
+        {
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2235});
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (0), 2235});
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secureasync <peer> (local);
+
+                co_await connection.sendasync(message);
+                co_await connection.sendasync(vector);
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 2)
+        {
+            auto listener = tcp :: listen(2235);
+            auto connection = listener.acceptsync();
+
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2235});
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secureasync <server> (local);
+
+                co_await connection.sendasync(message);
+
+                auto receivedvector = co_await connection.receiveasync <std :: vector <uint8_t>>();
+                if(receivedvector != vector)
+                    throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 3)
+        {
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2235});
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (2), 2235});
+
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secureasync <client> (local, remote);
+
+                auto receivedmessage = co_await connection.receiveasync <std :: string> ();
+                if(receivedmessage != message)
+                    throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+                co_await connection.sendasync(vector);
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 4)
+        {
+            auto listener = tcp :: listen(2235);
+            auto connection = listener.acceptsync();
+
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2235});
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secureasync <peer> (local, remote);
+
+                co_await connection.sendasync(message);
+
+                auto receivedvector = co_await connection.receiveasync <std :: vector <uint8_t>>();
+                if(receivedvector != vector)
+                    throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 5)
+        {
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2235});
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (4), 2235});
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secureasync <peer> (local, remote);
+
+                auto receivedmessage = co_await connection.receiveasync <std :: string> ();
+                if(receivedmessage != message)
+                    throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+                co_await connection.sendasync(vector);
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else
+        {
+            // Manager
+
+            std :: string goodbye  = "Goodbye!";
+
+            auto listener = tcp :: listen(2235);
+
+            auto one = listener.acceptsync();
+            auto two = listener.acceptsync();
+            auto three = listener.acceptsync();
+            auto four = listener.acceptsync();
+            auto five = listener.acceptsync();
+            auto six = listener.acceptsync();
+
+            one.receivesync <std :: string> ();
+            two.receivesync <std :: string> ();
+            three.receivesync <std :: string> ();
+            four.receivesync <std :: string> ();
+            five.receivesync <std :: string> ();
+            six.receivesync <std :: string> ();
+
+            one.send(goodbye);
+            two.send(goodbye);
+            three.send(goodbye);
+            four.send(goodbye);
+            five.send(goodbye);
+            six.send(goodbye);
+
+            sleep(1_s);
+        }
+    });
+
+    $test("connection/secure", {.instances = 7},[]
+    {
+        std :: string message = "Hello world!";
+        std :: vector <uint8_t> vector = {3, 2, 1, 0};
+
+        std :: string bye = "Bye.";
+
+        keyexchanger local;
+
+        if(:: test :: instance :: id() == 0)
+        {
+            auto listener = tcp :: listen(2236);
+            auto connection = listener.acceptsync();
+
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2236});
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secure <peer> (local);
+
+                auto receivedmessage = co_await connection.receiveasync <std :: string> ();
+                if(receivedmessage != message)
+                    throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+                auto receivedvector = co_await connection.receiveasync <std :: vector <uint8_t>>();
+                if(receivedvector != vector)
+                    throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 1)
+        {
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2236});
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (0), 2236});
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secure <peer> (local);
+
+                co_await connection.sendasync(message);
+                co_await connection.sendasync(vector);
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 2)
+        {
+            auto listener = tcp :: listen(2236);
+            auto connection = listener.acceptsync();
+
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2236});
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secure <server> (local);
+
+                co_await connection.sendasync(message);
+
+                auto receivedvector = co_await connection.receiveasync <std :: vector <uint8_t>>();
+                if(receivedvector != vector)
+                    throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 3)
+        {
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2236});
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (2), 2236});
+
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secure <client> (local, remote);
+
+                auto receivedmessage = co_await connection.receiveasync <std :: string> ();
+                if(receivedmessage != message)
+                    throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+                co_await connection.sendasync(vector);
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 4)
+        {
+            auto listener = tcp :: listen(2236);
+            auto connection = listener.acceptsync();
+
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2236});
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secure <peer> (local, remote);
+
+                co_await connection.sendasync(message);
+
+                auto receivedvector = co_await connection.receiveasync <std :: vector <uint8_t>>();
+                if(receivedvector != vector)
+                    throw "`receivesync <std :: vector <uint8_t>>` does not return the vector given to `sendsync`.";
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else if(:: test :: instance :: id() == 5)
+        {
+            sleep(1_s);
+            auto goodbye = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (6), 2236});
+            auto connection = tcp :: connectsync({:: test :: instance :: get <:: test :: IPv6> (4), 2236});
+
+            connection.sendsync(bytewise :: serialize(local.publickey()));
+            auto remote = bytewise :: deserialize <class keyexchanger :: publickey> (connection.receivesync <std :: array <uint8_t, bytewise :: traits :: size <class keyexchanger :: publickey> ()>> ());
+
+            [&]() -> promise <void>
+            {
+                co_await connection.secure <peer> (local, remote);
+
+                auto receivedmessage = co_await connection.receiveasync <std :: string> ();
+                if(receivedmessage != message)
+                    throw "`receivesync <std :: string>` does not return the string given to `sendsync`.";
+
+                co_await connection.sendasync(vector);
+
+                goodbye.sendsync(bye);
+            }();
+
+            goodbye.receivesync <std :: string> ();
+            sleep(1_s);
+        }
+        else
+        {
+            // Manager
+
+            std :: string goodbye  = "Goodbye!";
+
+            auto listener = tcp :: listen(2236);
+
+            auto one = listener.acceptsync();
+            auto two = listener.acceptsync();
+            auto three = listener.acceptsync();
+            auto four = listener.acceptsync();
+            auto five = listener.acceptsync();
+            auto six = listener.acceptsync();
+
+            one.receivesync <std :: string> ();
+            two.receivesync <std :: string> ();
+            three.receivesync <std :: string> ();
+            four.receivesync <std :: string> ();
+            five.receivesync <std :: string> ();
+            six.receivesync <std :: string> ();
+
+            one.send(goodbye);
+            two.send(goodbye);
+            three.send(goodbye);
+            four.send(goodbye);
+            five.send(goodbye);
+            six.send(goodbye);
+
+            sleep(1_s);
+        }
+    });
 };
