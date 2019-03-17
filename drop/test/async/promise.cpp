651f859abe51756376ce2eb197a7475cb2e36fbf
lromerio <lucio.romerio@epfl.ch>
2019-03-17 12:57:23 +0100

    Update async/promise test
    Add test for:
    - `resolvehard`
    - `resolvesoft`
    - `rejecthard`
    - `rejectsoft`

--- drop/test/async/promise.cpp
+++ drop/test/async/promise.cpp
@@ -54,6 +54,152 @@ namespace
 
     // Tests
 
+    $test("promise/resolvehard", []
+    {
+        g();
+
+        for(int i = 33; i < 43; i++)
+        {
+            x.resolvehard(i);
+            if(lastval != i)
+                throw "Calling `resolvehard` on `promise <int>` did not correctly resume coroutine `f`.";
+        }
+
+        thrown = false;
+
+        try
+        {
+            x.resolvehard(9);
+            x.resolvehard(10);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+
+        if(!thrown)
+            throw "Calling `resolvehard` twice on a `promise` does not raise an exception.";
+
+        f();
+        if(lastval != 9)
+            throw "Making a coroutine await on a `promise` that was already hard-resolved does not cause it to pass through.";
+
+        lastval = 0;
+
+        f();
+        if(lastval != 9)
+            throw "Making a coroutine await twice on a `promise` that was already hard-resolved does not cause it to pass through.";
+
+        resumes = 0;
+        x = promise <int> ();
+
+        f();
+        f();
+        f();
+        f();
+
+        x.resolvehard(99);
+        if(lastval != 99)
+            throw "Hard-esolving a `promise` on which multiple coroutines are awaiting fails.";
+
+        if(resumes != 4)
+            throw "Hard-resolving a `promise` on which multiple coroutines are awaiting does not resume all of them.";
+    });
+
+    $test("promise/resolvesoft", []
+    {
+        g();
+
+        for(int i = 33; i < 43; i++)
+        {
+            x.resolvesoft(i);
+            if(lastval != i)
+                throw "Calling `resolvesoft` on `promise <int>` did not correctly resume coroutine `f`.";
+        }
+
+        thrown = false;
+        try
+        {
+            x.resolve(9);
+            x.resolvesoft(10);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+        if(thrown)
+            throw "Calling `resolvesoft` on `promise` that was previously resolved raises an exception.";
+
+        x = promise <int> ();
+        h();
+        thrown = false;
+        try
+        {
+            x.rejectsoft("Soft");
+            x.resolvesoft(10);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+        if(thrown)
+            throw "Calling `resolvesoft` on a `promise` that was previously soft-rejected raises an exception.";
+
+        x = promise <int> ();
+        h();
+        thrown = false;
+        try
+        {
+            x.reject("Soft");
+            x.resolvesoft(10);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+        if(thrown)
+            throw "Calling `resolvesoft` on a `promise` that was previously rejected raises an exception.";
+
+        x = promise <int> ();
+        thrown = false;
+        try
+        {
+            x.resolvesoft(9);
+            x.resolvesoft(10);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+        if(thrown)
+            throw "Calling `resolvesoft` twice on a `promise` raises an exception.";
+
+        f();
+        if(lastval != 9)
+            throw "Making a coroutine await on a `promise` that was already soft-resolved does not cause it to pass through.";
+
+        lastval = 0;
+
+        f();
+        if(lastval != 9)
+            throw "Making a coroutine await twice on a `promise` that was already soft-resolved does not cause it to pass through.";
+
+        resumes = 0;
+        x = promise <int> ();
+
+        f();
+        f();
+        f();
+        f();
+
+        x.resolvesoft(99);
+        if(lastval != 99)
+            throw "Soft-esolving a `promise` on which multiple coroutines are awaiting fails.";
+
+        if(resumes != 4)
+            throw "Soft-resolving a `promise` on which multiple coroutines are awaiting does not resume all of them.";
+    });
+
     $test("promise/resolve", []
     {
         g();
@@ -104,6 +250,160 @@ namespace
 
         if(resumes != 4)
             throw "Resolving a `promise` on which multiple coroutines are awaiting does not resume all of them.";
+    });
+
+    $test("promise/rejecthard", []
+    {
+        h();
+
+        thrown = false;
+        x.rejecthard("Ouch!");
+
+        if(!thrown)
+            throw "Hard-rejecting a `promise` does not cause the exception to appropriately propagate.";
+
+        thrown = false;
+        x = promise <int> ();
+        x.rejecthard("Ouch!");
+
+        h();
+
+        if(!thrown)
+            throw "Making a coroutine await on an already hard-rejected `promise` does not cause it to pass through.";
+
+        thrown = false;
+
+        try
+        {
+            x.rejecthard(33);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+
+        if(!thrown)
+            throw "Rejecting a `promise` twice does not raise an exception.";
+
+        thrown = false;
+
+        try
+        {
+            x.resolve(144);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+
+        if(!thrown)
+            throw "Resolving a `promise` that was previously hard-rejected does not raise an exception.";
+    });
+
+    $test("promise/rejectsoft", []
+    {
+        h();
+
+        thrown = false;
+        x.rejectsoft("Ouch!");
+
+        if(!thrown)
+            throw "Soft-rejecting a `promise` does not cause the exception to appropriately propagate.";
+
+        thrown = false;
+        x = promise <int> ();
+        x.rejectsoft("Ouch!");
+
+        h();
+
+        if(!thrown)
+            throw "Making a coroutine await on an already soft-rejected `promise` does not cause it to pass through.";
+
+        thrown = false;
+
+        try
+        {
+            x.rejectsoft(33);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+
+        if(thrown)
+            throw "Soft-rejecting a `promise` twice raises an exception.";
+
+        thrown = false;
+
+        try
+        {
+            x.resolve(144);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+
+        if(!thrown)
+            throw "Resolving a `promise` that was previously soft-rejected does not raise an exception.";
+
+        x = promise <int> ();
+        thrown = false;
+        try
+        {
+            x.resolve(9);
+            x.rejectsoft("Soft");
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+        if(thrown)
+            throw "Calling `rejectsoft` on a `promise` that was previously resolved raises an exception.";
+
+        x = promise <int> ();
+        h();
+        thrown = false;
+        try
+        {
+            x.rejectsoft("Soft");
+            x.resolvesoft(10);
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+        if(thrown)
+            throw "Calling `resolvesoft` twice a `promise` raises an exception.";
+
+        x = promise <int> ();
+        h();
+        thrown = false;
+        try
+        {
+            x.reject("Soft");
+            x.rejectsoft("Soft");
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+        if(thrown)
+            throw "Calling `rejectsoft` on a `promise` that was previously rejected raises an exception.";
+
+        x = promise <int> ();
+        thrown = false;
+        try
+        {
+            x.resolvesoft(9);
+            x.rejectsoft("Soft");
+        }
+        catch(exception <bad_access, already_resolved> &)
+        {
+            thrown = true;
+        }
+        if(thrown)
+            throw "Calling `rejectsoft` twice on a `promise` that was previously soft-resolced raises an exception.";
     });
 
     $test("promise/reject", []
